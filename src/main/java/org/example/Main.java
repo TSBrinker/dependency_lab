c6ec57512c16ee36d46456c5447eb68e3c5262ac
TSBrinker <tsbrinker@gmail.com>
2022-11-22 13:50:01 -0600

    instantiated second Dinosaur Attacker in main, added Attack method for all Attackers, finished lab

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello there!");
 
-        Sword excalibur = new Sword();
-        Bow bow = new Bow();
-        Axe axe = new Axe();
+        Weapon excalibur = new Sword();
+        Weapon bow = new Bow();
+        Weapon axe = new Axe();
 
         Attacker robot_one = new Robot("Hogarth", excalibur);
         Attacker robot_two = new Robot("Dominus", bow);
@@ -17,8 +17,10 @@ public class Main {
         robot_three.Attack();
 
         Attacker dinosaur_one = new Dinosaur("Yoshi", 100);
+        Attacker dinosaur_two = new Dinosaur("Ancalagon", 1000);
 
         dinosaur_one.Attack();
+        dinosaur_two.Attack();
     }
 
 
